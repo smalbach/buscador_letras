45136e9c4a9c0adbfc8aee48cde6ba8aed28a61e
Snheidert Smalbach <smalbach24@gmail.com>
2020-11-02 22:02:06 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from "react";
+import Formulario from "./components/Formulario";
+import Cancion from "./components/Cancion";
+import Info from "./components/Info";
+import axios from "axios";
 
 function App() {
+  //definimos el state
+  const [busquedaletra, guardarBusuqedaLetra] = useState({});
+  const [letra, guardarLetra] = useState("");
+  const [info, guardarInfo] = useState({});
+
+  useEffect(() => {
+    if (Object.keys(busquedaletra).length === 0) return;
+
+    const consultarApiLetra = async () => {
+      const { artista, cancion } = busquedaletra;
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+      const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+
+      const [letra, informacion] = await Promise.all([
+        axios.get(url),
+        axios.get(url2),
+      ]);
+      console.log(letra.data.lyrics);
+      guardarLetra(letra.data.lyrics);
+      guardarInfo(informacion.data.artists[0]);
+    };
+    consultarApiLetra();
+  }, [busquedaletra]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Formulario guardarBusuqedaLetra={guardarBusuqedaLetra} />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <Info info={info} />
+          </div>
+          <div className="col-md-6">
+            <Cancion letra={letra} />
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/Formulario.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+
+const Formulario = ({ guardarBusuqedaLetra }) => {
+  const [busqueda, guardarBusqueda] = useState({
+    artista: "",
+    cancion: "",
+  });
+
+  const [error, guardarError] = useState(false);
+
+  const { artista, cancion } = busqueda;
+
+  //Funcion a cada input
+  const actualizarState = (e) => {
+    guardarBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const buscarInformacion = (e) => {
+    e.preventDefault();
+    guardarError(false);
+
+    if (artista.trim() === "" || cancion === "") {
+      guardarError(true);
+      return;
+    }
+
+    guardarBusuqedaLetra(busqueda);
+  };
+
+  return (
+    <div className="bg-info">
+      {error ? (
+        <p className="alert alert-danger text-center p-2">
+          Todos los campos son obligatorias
+        </p>
+      ) : null}
+      <div className="container">
+        <div className="row">
+          <form
+            className="col card text-white bg-transparent mb-5 pt-5 pb-2"
+            onSubmit={buscarInformacion}
+          >
+            <fieldset>
+              <legend className="text-center">
+                Buscador letras de canciones
+              </legend>
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label>Artista</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="artista"
+                      placeholder="Nombre del artista"
+                      onChange={actualizarState}
+                      value={artista}
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label>Cancion</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="cancion"
+                      placeholder="Nombre de la cancion"
+                      onChange={actualizarState}
+                      value={cancion}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <button type="submit" className="btn btn-primary float-right">
+                Buscar
+              </button>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Formulario;
